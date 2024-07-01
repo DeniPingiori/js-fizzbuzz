6c4c7149024b443e888428c17d48a2fa0d7ef9c3
Denise Pingiori <pingiorid@gmail.com>
2024-07-01 17:26:18 +0200

    [upd] upd second method

--- js/script.js
+++ js/script.js
@@ -17,5 +17,21 @@ for(let i=1; i<=100; i++){
     else{
         console.log(i);
     }
+    
+    //oppure 
+ /*   if(i % 3 ==0 && i % 5 ==0) {
+        console.log('Fizz');
+    }
+    //verifico se il num attualmente iterato è multiplo di 5 
+    else if(i % 5 ==0) {
+        console.log('Buzz')
+    }
+    //verifico se il num attualmente iterato è multiplo di 3 e di 5
+    else if(i % 3 ==0){
+        console.log('Fizzbuzz') //non entra nella condizione perchè incontra un'altra condizione prec
+    }
+    else{
+        console.log(i);
+    }
 }
-
+*/
